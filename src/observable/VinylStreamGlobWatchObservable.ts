184de7f141e9cbaf83cb296bdc68f10c4a23f269
Stéphane Le Dorze <stephane.ledorze@gmail.com>
2017-08-30 13:50:18 +0200

    add support to discard content from watch globs

--- src/observable/VinylStreamGlobWatchObservable.ts
+++ src/observable/VinylStreamGlobWatchObservable.ts
@@ -5,13 +5,15 @@ import { Observable } from 'rxjs/Observable';
 import { Subscriber } from 'rxjs/Subscriber';
 import { TeardownLogic } from 'rxjs/Subscription';
 
-import { WatchOptions } from 'chokidar'
+import { WatchOptions as ChokidarOptions } from 'chokidar'
 
 import * as Vinyl from 'vinyl';
 import * as fs from 'fs';
 const watch = require('glob-watcher');
 
-export { WatchOptions }
+
+export type WatchOptions = ChokidarOptions & { read?: boolean }
+
 
 export class VinylStreamGlobWatchObservable extends Observable<Vinyl> {
 
@@ -27,21 +29,29 @@ export class VinylStreamGlobWatchObservable extends Observable<Vinyl> {
 		const watcher = watch(this.glob, this.options);
 		const scheduler = this.scheduler;
 
-		const next = (path: string) => {
+		const mustRead = this.options ? this.options.read !== false : true
 
-			fs.readFile(path, (err, data) => {
-				if (err) {
-					scheduler == null
-						? subscriber.error(err)
-						: !subscriber.closed && subscriber.add(scheduler.schedule(subscriber.error, 0, err));
-				}
-				else {
-					scheduler == null
-						? subscriber.next(new Vinyl({ path: path, contents: data }))
-						: !subscriber.closed && subscriber.add(scheduler.schedule(subscriber.next, 0, new Vinyl({ path: path, contents: data })));
-				}
-			});
-		};
+		const next =
+			!mustRead ?
+				(
+					(path: string) =>
+						scheduler == null
+							? subscriber.next(new Vinyl({ path: path }))
+							: !subscriber.closed && subscriber.add(scheduler.schedule(subscriber.next, 0, new Vinyl({ path: path })))
+				) : (
+					(path: string) =>
+						fs.readFile(path, (err, data) => {
+							if (err) {
+								scheduler == null
+									? subscriber.error(err)
+									: !subscriber.closed && subscriber.add(scheduler.schedule(subscriber.error, 0, err));
+							} else {
+								scheduler == null
+									? subscriber.next(new Vinyl({ path: path, contents: data }))
+									: !subscriber.closed && subscriber.add(scheduler.schedule(subscriber.next, 0, new Vinyl({ path: path, contents: data })));
+							}
+						})
+				)
 
 		const unlink = (path: string) => {
 			scheduler == null
